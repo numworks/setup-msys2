bce3cf9b39bd2b12787a89d695aba37996749280
1138-4EB <1138-4EB@users.noreply.github.com>
2019-10-07 00:07:00 +0200

    style: remove non-required variables

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ async function run() {
       core.setFailed("MSYS2 does not work on non-windows platforms; please check the 'runs-on' field of the job");
     }
 
-    const msys2base = await tc.downloadTool('http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20190524.tar.xz');
     const tar = await io.which('tar', true);
-
     const dest = path.join(process.env['RUNNER_TEMP'], 'msys')
-
-    // For some reason, GNU Tar on Windows expects paths to be slash-separated
-    const normalizedDest = dest.replace(/\\/g, '/')
 
     await io.mkdirP(dest)
 
-    await exec.exec(`"${tar}"`, ['-x', '-J', '--force-local', '-C', normalizedDest, '-f', msys2base])
+    await exec.exec(`"${tar}"`, [
+      '-x', '-J', '--force-local',
+      // For some reason, GNU Tar on Windows expects paths to be slash-separated
+      '-C', dest.replace(/\\/g, '/'),
+      '-f', await tc.downloadTool('http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20190524.tar.xz')
+    ])
 
     let cmd = path.join(dest, 'msys2do.cmd')
     fs.writeFileSync(cmd, fs.readFileSync(path.join(__dirname, 'msys2do.in')))
